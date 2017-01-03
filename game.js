b051a210a33db41ca8c88a4a240952a0c9bd2c7d
kpatrick <none>
2017-01-02 22:20:50 -0800

    more work

--- game.js
+++ game.js
@@ -4,15 +4,33 @@ BasicGame.Game = function (game) {
 };
 
 BasicGame.Game.prototype = {
-
+  preload: function() {
+    this.load.image("sea", "assets/sea.png");
+    this.load.image("bullet", "assets/bullet.png");
+    this.load.spritesheet("greenEnemy", "assets/enemy.png", 32, 32);
+  },
+  
   create: function () {
-
     this.sea = this.add.tileSprite(0, 0, 800, 600, 'sea');
-
+    this.enemy = this.add.sprite(400, 200, "greenEnemy");
+    this.enemy.anchor.setTo(0.5, 0.5);
+    this.physics.enable(this.enemy, Phaser.Physics.ARCADE)
+    this.enemy.animations.add("fly", [0, 1, 2], 20, true);
+    this.enemy.play("fly");
+    this.bullet = this.add.sprite(400, 300, "bullet");
+    this.bullet.anchor.setTo(0.5, 0.5);
+    this.physics.enable(this.bullet, Phaser.Physics.ARCADE);
+    this.bullet.body.velocity.y = -500;
   },
 
   update: function () {
     //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+    this.sea.tilePosition.y += 0.2;    
+  },
+  
+  render: function(){
+    this.game.debug.body(this.bullet);
+    this.game.debug.body(this.enemy);
   },
 
   quitGame: function (pointer) {
